b4dfc00a6292f7fbbb6f03d027601bcc4a578b27
Brent Marshall <info@brentmarshall.tech>
2025-01-25 18:00:22 -0500

    LangChain + Gemini initial commit

---
+++ ticket/ticket_data.py
@@ -0,0 +1,28 @@
+concert_tickets = {
+    "events": [
+        {
+            "id": "1",
+            "artist": "Taylor Swift",
+            "venue": "Madison Square Garden",
+            "date": "2025-03-15",
+            "time": "20:00",
+            "available_tickets": [
+                {"section": "A1", "row": "1", "price": 350.00, "quantity": 4},
+                {"section": "B2", "row": "5", "price": 250.00, "quantity": 8},
+                {"section": "C3", "row": "10", "price": 150.00, "quantity": 12}
+            ]
+        },
+        {
+            "id": "2",
+            "artist": "Ed Sheeran",
+            "venue": "Barclays Center",
+            "date": "2025-04-20",
+            "time": "19:30",
+            "available_tickets": [
+                {"section": "Floor", "row": "2", "price": 300.00, "quantity": 6},
+                {"section": "Lower Bowl", "row": "8", "price": 200.00, "quantity": 10},
+                {"section": "Upper Bowl", "row": "15", "price": 100.00, "quantity": 15}
+            ]
+        }
+    ]
+}
